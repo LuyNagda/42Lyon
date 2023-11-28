b1ec73f5d4a2aa7759dd003979afaf98983fec98
Luy Nagda <lunagda@z4r3p1.42lyon.fr>
2023-11-28 15:27:07 +0100

    Working algo

--- push_swap/solving/solve.c
+++ push_swap/solving/solve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solve.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luynagda <luynagda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 11:47:27 by lunagda           #+#    #+#             */
-/*   Updated: 2023/11/27 20:25:13 by luynagda         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:24:46 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	solve(t_list **stack_a, t_list **stack_b)
 {
 	int		cheapest;
 	int		distance;
+	int		distance_b;
 	int		r_distance;
-	t_list	*head;
+	int		r_distance_b;
+	//t_list	*head;
 
 	if (ft_lstsize(*stack_a) == 3)
 		solve_for_3(stack_a);
@@ -28,43 +30,108 @@ void	solve(t_list **stack_a, t_list **stack_b)
 		pb(stack_a, stack_b);
 		pb(stack_a, stack_b);
 	}
-	head = *stack_a;
+	//t_list	*head2 = *stack_b;
+	//printf("stack b======================================================\n");
+	//while (head2)
+	//{
+	//	printf("%d\n", head2->content);
+	//	head2 = head2->next;
+	//}
+	//printf("stack b ends=================================================\n");
+	//head = *stack_a;
+	//printf("stack a======================================================\n");
+	//while (head)
+	//{
+	//	printf("%d\n", head->content);
+	//	head = head->next;
+	//}
+	//printf("stack a ends=================================================\n");
+	//head = *stack_a;
 	distance = 0;
+	distance_b = 0;
+	r_distance_b = 0;
 	r_distance = 0;
+	if ((*stack_b)->content == get_min(stack_b, -1))
+		rb(stack_b);
 	cheapest = get_cheapest(stack_a, stack_b);
 	distance = get_distance(stack_a, cheapest);
-	if (head->content > get_max(stack_b, -1) || head->content < get_min(stack_b, -1))
+	if (cheapest > get_max(stack_b, -1) || cheapest < get_min(stack_b, -1))
 	{
-		if ((*stack_b)->content == get_max(stack_b, -1))
+		distance = get_distance(stack_a, cheapest);
+		r_distance = ft_lstsize(*stack_a) - distance;
+		//printf("1cheapest: %d\n", cheapest);
+		//printf("1to_find: %d\n", to_find(stack_b, cheapest));
+		//printf("1distance: %d\n", distance);
+		//printf("1r_distance: %d\n", r_distance);
+		if (distance < r_distance)
+		{
+			while (distance)
+			{
+				ra(stack_a);
+				distance--;
+			}
 			pb(stack_a, stack_b);
+		}
 		else
 		{
-			distance = get_distance(stack_a, get_max(stack_b, -1));
-			r_distance = ft_lstsize(*stack_b) - distance;
-			if (distance < r_distance)
+			while (r_distance)
 			{
-				while (distance)
-				{
-					rb(stack_b);
-					distance--;
-				}
-				pb(stack_a, stack_b);
+				rra(stack_a);
+				r_distance--;
 			}
-			else
-			{
-				while (r_distance)
-				{
-					rrb(stack_b);
-					r_distance--;
-				}
-				pb(stack_a, stack_b);
-			}
+			pb(stack_a, stack_b);
 		}
 	}
 	else
 	{
-		distance = get_distance(stack_b, to_find(stack_b, cheapest));
-		r_distance = (ft_lstsize(*stack_b) - distance);
+		distance = get_distance(stack_a, cheapest);
+		r_distance = (ft_lstsize(*stack_a) - distance);
+		distance_b = get_distance(stack_b, to_find(stack_b, cheapest));
+		r_distance_b = (ft_lstsize(*stack_b) - distance_b);
+		//printf("2cheapest: %d\n", cheapest);
+		//printf("2to_find: %d\n", to_find(stack_b, cheapest));
+		//printf("2distance: %d\n", distance);
+		//printf("2r_distance: %d\n", r_distance);
+		if (distance < r_distance)
+		{
+			while (distance)
+			{
+				ra(stack_a);
+				distance--;
+			}
+		}
+		else
+		{
+			while (r_distance)
+			{
+				rra(stack_a);
+				r_distance--;
+			}
+		}
+		if (distance_b < r_distance_b)
+		{
+			while (distance_b)
+			{
+				rb(stack_b);
+				distance_b--;
+			}
+			pb(stack_a, stack_b);
+		}
+		else
+		{
+			while (r_distance_b)
+			{
+				rrb(stack_b);
+				r_distance_b--;
+			}
+			pb(stack_a, stack_b);
+		}
+	}
+	solve(stack_a, stack_b);
+	if (ft_lstsize(*stack_a) == 3)
+	{
+		distance = get_distance(stack_b, get_max(stack_b, -1));
+		r_distance = ft_lstsize(*stack_b) - distance;
 		if (distance < r_distance)
 		{
 			while (distance)
@@ -72,8 +139,6 @@ void	solve(t_list **stack_a, t_list **stack_b)
 				rb(stack_b);
 				distance--;
 			}
-			pb(stack_a, stack_b);
-			rrb(stack_b);
 		}
 		else
 		{
@@ -82,15 +147,7 @@ void	solve(t_list **stack_a, t_list **stack_b)
 				rrb(stack_b);
 				r_distance--;
 			}
-			pb(stack_a, stack_b);
-			rb(stack_b);
 		}
-	}
-	solve(stack_a, stack_b);
-	if (ft_lstsize(*stack_a) == 3)
-	{
-		while ((ft_lstlast(stack_a)->content) > ((*stack_b)->content))
-			rra(stack_a);
 		while (ft_lstsize(*stack_b))
 		{
 			distance = get_distance(stack_a, r_to_find(stack_a, (*stack_b)->content));
--- push_swap/utils/utils.c
+++ push_swap/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 16:27:02 by lunagda           #+#    #+#             */
-/*   Updated: 2023/11/27 17:53:27 by lunagda          ###   ########.fr       */
+/*   Updated: 2023/11/28 15:25:35 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,19 @@ int	to_find(t_list **stack_b, int val)
 	t_list	*head;
 	int		tmp;
 
-	head = *stack_b;
-	while (head)
+	tmp = val;
+	while (tmp)
 	{
-		tmp = val;
-		while (tmp)
+		head = *stack_b;
+		while (head)
 		{
 			if (head->content == tmp)
+			{
 				return (tmp);
-			tmp--;
+			}
+			head = head->next;
 		}
-		head = head->next;
+		tmp--;
 	}
 	return (0);
 }
@@ -71,10 +73,14 @@ int	r_to_find(t_list **stack_a, int val)
 	while (42)
 	{
 		head = *stack_a;
+		if (get_max(stack_a, -1) < val)
+			return (head->content);
 		while (head)
 		{
 			if (head->content == val)
+			{
 				return (val);
+			}
 			head = head->next;
 		}
 		val++;
@@ -105,6 +111,7 @@ int	get_cheapest(t_list **stack_a, t_list **stack_b)
 {
 	int		count;
 	int		distance;
+	int		r_distance;
 	int		cheapest;
 	int		return_val;
 	t_list	*head;
@@ -112,30 +119,30 @@ int	get_cheapest(t_list **stack_a, t_list **stack_b)
 	return_val = 0;
 	cheapest = 0;
 	distance = 0;
+	r_distance = 0;
 	head = *stack_a;
 	while (head)
 	{
 		count = 0;
-		if (head->content > get_max(stack_b, head->content) || head->content < get_min(stack_b, -1))
+		if (head->content > get_max(stack_b, -1) || head->content < get_min(stack_b, -1))
 		{
-			if ((*stack_b)->content == get_max(stack_b, -1))
-				count += get_distance(stack_a, head->content);
+			distance = get_distance(stack_a, get_max(stack_b, 1));
+			r_distance = ft_lstsize(*stack_a) - distance;
+			if (distance < r_distance)
+				count += distance + 1;
 			else
-			{
-				distance = get_distance(stack_b, get_max(stack_b, 1));
-				if (distance < (ft_lstsize(*stack_b) - distance))
-					count += distance + 1 + get_distance(stack_a, head->content);
-				else
-					count += (ft_lstsize(*stack_b) - distance) + 1 + get_distance(stack_a, head->content);
-			}
+				count += r_distance + 1;
 		}
 		else
 		{
-			distance = get_distance(stack_b, to_find(stack_b, head->content));
-			if (distance < (ft_lstsize(*stack_b) - distance))
-				count += distance + 1 + get_distance(stack_a, head->content);
+			distance = get_distance(stack_b, to_find(stack_b, head->content))
+					+ get_distance(stack_a, to_find(stack_a, head->content));
+			r_distance = (get_distance(stack_b, to_find(stack_b, head->content))
+					+ ft_lstsize(*stack_a)) - distance;
+			if (distance < r_distance)
+				count += distance + 1;
 			else
-				count += (ft_lstsize(*stack_b) - distance) + 1 + get_distance(stack_a, head->content);
+				count += r_distance + 1;
 		}
 		if (count < cheapest || cheapest == 0)
 		{
