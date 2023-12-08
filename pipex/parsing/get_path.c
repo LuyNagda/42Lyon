b6e9095c9012d21c2e13dca621bfb900a14e95cb
Luy Nagda <lunagda@z4r4p3.42lyon.fr>
2023-12-08 16:00:34 +0100

    Fixing malloc issue with strjoin_free

--- pipex/parsing/get_path.c
+++ pipex/parsing/get_path.c
@@ -6,11 +6,36 @@
 /*   By: lunagda <lunagda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:38:19 by lunagda           #+#    #+#             */
-/*   Updated: 2023/12/08 13:16:43 by lunagda          ###   ########.fr       */
+/*   Updated: 2023/12/08 15:57:57 by lunagda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
+
+char	*ft_strjoin_free(char const *s1, char const *s2)
+{
+	char	*trg;
+	int		i;
+	int		s1_len;
+	int		s2_len;
+
+	i = 0;
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
+	trg = (char *)malloc((s1_len + s2_len) * sizeof(char) + 1);
+	if (!trg)
+		return (free((char *)s1), NULL);
+	while (s1[i])
+	{
+		trg[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+		trg[s1_len++] = s2[i++];
+	trg[s1_len] = '\0';
+	return (free((char *)s1), trg);
+}
 
 char	*get_path(char **paths, char **cmd)
 {
@@ -26,11 +51,11 @@ char	*get_path(char **paths, char **cmd)
 	}
 	while (paths[i])
 	{
-		paths[i] = ft_strjoin(paths[i], "/");
+		paths[i] = ft_strjoin_free(paths[i], "/");
 		path = ft_strjoin(paths[i], cmd[0]);
-		if (access(ft_strjoin(paths[i], cmd[0]), F_OK) == 0
-			&& access(ft_strjoin(paths[i], cmd[0]), X_OK) == 0)
-			return (ft_strjoin(paths[i], cmd[0]));
+		if (access(path, F_OK) == 0
+			&& access(path, X_OK) == 0)
+			return (path);
 		free(path);
 		i++;
 	}
